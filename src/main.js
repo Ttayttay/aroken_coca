a6bb924baf0750b50cf93214bb46e32e4bb42ced
Andrew Tabakov <ttayttay1452@gmail.com>
2024-08-19 18:12:03 +0500

    added header

--- src/main.js
+++ src/main.js
@@ -1,1 +1,8 @@
 import '/assets/scss/main.scss';
+
+// components
+import { useTheme } from './js/components/theme.js';
+import { useBurger } from './js/components/burger.js';
+
+useTheme();
+useBurger();

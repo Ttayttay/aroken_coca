383a3a2fe8809fa5e6e73ec12901a7b931e606f3
Andrew Tabakov <ttayttay1452@gmail.com>
2024-08-21 18:09:43 +0500

    added another part of site

---
+++ src/js/components/slider.js
@@ -0,0 +1,37 @@
+import Swiper from 'swiper';
+import { Navigation, EffectFade } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+
+export const useInsightSlider = () => {
+  new Swiper('.insight__slider', {
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    loop: true,
+    centeredSlides: true,
+    breakpoints: {
+      993: {
+        centeredSlides: false,
+      },
+    },
+  });
+};
+
+export const useTestimonialsSlider = () => {
+  new Swiper('.testimonials__slider', {
+    modules: [Navigation, EffectFade],
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+    slidesPerView: 1,
+    spaceBetween: 22,
+    loop: true,
+    navigation: {
+      prevEl: '.testimonials__btn--prev',
+      nextEl: '.testimonials__btn--next',
+    },
+  });
+};
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import '/assets/scss/main.scss';
+import './assets/scss/main.scss';
 
 // components
 import { useTheme } from './js/components/theme.js';
 import { useBurger } from './js/components/burger.js';
+import {
+  useInsightSlider,
+  useTestimonialsSlider,
+} from './js/components/slider.js';
 
 useTheme();
 useBurger();
+useInsightSlider();
+useTestimonialsSlider();

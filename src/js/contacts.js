4ea3257639fd13c21ea8d82140975857a35983e1
Andrew Tabakov <ttayttay1452@gmail.com>
2024-08-22 14:59:40 +0500

    Deploy

--- src/js/contacts.js
+++ src/js/contacts.js
@@ -1,10 +1,12 @@
-import '../assets/scss/contacts.scss';
+import '../scss/contacts.scss';
 
 // components
 import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useFooterAccordion } from './footer_accordion.js';
+import { usePhone } from './contacts/phone.js';
 
 useTheme();
 useBurger();
 useFooterAccordion();
+usePhone();

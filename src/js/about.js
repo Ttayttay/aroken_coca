744cec531428b2a176dc6323ecb73c005f58e83d
Andrew Tabakov <ttayttay1452@gmail.com>
2024-08-22 18:12:32 +0500

    Start making About page

---
+++ src/js/about.js
@@ -0,0 +1,10 @@
+import '../scss/about.scss';
+
+// components
+import { useTheme } from './components/theme.js';
+import { useBurger } from './components/burger.js';
+import { useFooterAccordion } from './footer_accordion.js';
+
+useTheme();
+useBurger();
+useFooterAccordion();
